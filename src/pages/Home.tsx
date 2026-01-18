df9c1269928339020cb1baf76e62e4fc0ac344b2
Alison de la Paz <arhibschle@gmail.com>
2026-01-18 13:05:52 -0500

    tailwind styling to homepage hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
 function Home() {
     return ( 
-       <main>
-        <h1>R&R Publishing</h1>
-        <h2>Rama y Raices</h2>
-        <p>Contemporary choral music for ensembles of all sizes.</p>
-        <section className="hero"></section>
+       <main className="max-w-5xl mx-auto px-6 py-12">
+        <section className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight">R&R Publishing</h1>
+        <h2 className="mt-2 text-xl text-grat-600">Rama y Raices</h2>
+        <p className="mt-6 text-lg text-gray-700 max-w-2xl mx-auto">Contemporary choral music for ensembles of all sizes.</p>
+        </section>
+        {/* Hero Section */}
+       <section className="mt-12 h-64 bg-gray-200 flex items-center justify-center rounded-md">
+        <span className="text-gray-500">Hero Image Placeholder</span>
+       </section>
        </main>
     )
 }
