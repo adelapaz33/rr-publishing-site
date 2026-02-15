03a7c8279dfe8ad6cd8fc35478d4d78951b28aee
Alison de la Paz <arhibschle@gmail.com>
2026-02-15 15:33:12 -0600

    music page update

--- src/pages/MusicCatalog.tsx
+++ src/pages/MusicCatalog.tsx
@@ -2,14 +2,14 @@ import scores from "../data/scores.json";
 import MusicCard from "../components/cards/MusicCard";
 
 function MusicCatalog({ }) {
-    return <section className="catalog">
+    return <section className="catalog bg-[#FFF8F0]">
     <h1 className="text-4xl font-bold mb-8 text-center">Music Catalog</h1>
     <ul>{scores.map((score) => (
         <MusicCard
         key={score.id}
         title={score.title}
         composer={score.composer}
-        arranger={score.arranger}
+        arranger={score.arranged}
         voicing={score.voicing}
         cover={score.cover}
 
