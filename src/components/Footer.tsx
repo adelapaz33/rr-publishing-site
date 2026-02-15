0c4e6ee42a098eaac1cba1a7bb4aa77c21dbfacc
Alison de la Paz <arhibschle@gmail.com>
2026-02-15 15:28:03 -0600

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,36 @@
 import { Link } from "react-router-dom";
 function Footer() {
     return( 
-        <footer>
+        <footer className="bg-[#1E3A8A] p-4" >
 <div className="flex flex-row justify-evenly" >
-<div className="flex flex-col">
+<div className="flex flex-col text-[#DBEAFE]">
         <h3>R&R Publishing</h3>
         <p>Championing Latin American choral composers since 2026</p>
         {/* add logos for email, linkedin, youtube, instagram/facebook  */}
     </div>
-    <div className="flex flex-col">
-        <h3>Catalog</h3>
-        <Link to="">Sacred Works</Link>
-        <Link to="">Secular Works</Link>
-        <Link to="">Folk Arrangements</Link>
-        <Link to="">Contemporary</Link>
+    <div className="flex flex-col text-white cursor-pointer">
+        <h3 className="hover:text-[#F4C430]">Catalog</h3>
+        <Link to="" className="hover:text-[#F4C430]">Sacred Works</Link>
+        <Link to="" className="hover:text-[#F4C430]">Secular Works</Link>
+        <Link to="" className="hover:text-[#F4C430]">Folk Arrangements</Link>
+        <Link to="" className="hover:text-[#F4C430]">Contemporary</Link>
     </div>
-    <div className="flex flex-col">
-        <h3>Resources</h3>
-        <Link to="">Composers</Link>
-        <Link to="">Pronunciation Guide</Link>
-        <Link to="">Audio Samples</Link>
-        <Link to="">Performance Notes</Link>
+    <div className="flex flex-col text-white cursor-pointer">
+        <h3 className="hover:text-[#F4C430]">Resources</h3>
+        <Link to="" className="hover:text-[#F4C430]">Composers</Link>
+        <Link to="" className="hover:text-[#F4C430]">Pronunciation Guide</Link>
+        <Link to="" className="hover:text-[#F4C430]">Audio Samples</Link>
+        <Link to="" className="hover:text-[#F4C430]">Performance Notes</Link>
     </div>
-    <div className="flex flex-col">
-        <h3>Company</h3>
-        <Link to="">About</Link>
-        <Link to="">Contact</Link>
-        <Link to="">Licensing</Link>
-        <Link to="">Privacy Policy</Link>
+    <div className="flex flex-col text-white cursor-pointer">
+        <h3 className="hover:text-[#F4C430]">Company</h3>
+        <Link to="" className="hover:text-[#F4C430]">About</Link>
+        <Link to="" className="hover:text-[#F4C430]">Contact</Link>
+        <Link to="" className="hover:text-[#F4C430]">Licensing</Link>
+        <Link to="" className="hover:text-[#F4C430]">Privacy Policy</Link>
     </div>
 </div>
-<div>
+<div className="text-[#DBEAFE]" >
     <p>@copy; 2026 R&R Publishing. All Rights Reserved.</p>
 </div>
         </footer>
--- src/components/cards/FeaturedScoresCard.tsx
+++ src/components/cards/FeaturedScoresCard.tsx
@@ -2,7 +2,7 @@ function FeaturedScoresCard({title, composer, arranger, cover}) {
     return(
         <div className="shadow rounded-xl max-w-[300px] overflow-hidden">
             <img className="h-75 w-full object-cover" src={cover} alt={title} />
-            <div className="p-4 text-left"> 
+            <div className="p-4 text-left bg-white"> 
             {/* link to purchase of song  */}
 <h3 className="text-lg" >{title}</h3> 
 {/* link to composer page  */}
